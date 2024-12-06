a5ad93f7882d48699d83270a4456f3b4e814fe3e
Fatih Zaidan Aqil <167433085+FatihSupremacy@users.noreply.github.com>
2024-12-07 00:00:14 +0700

    Update loadModel.js

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -2,6 +2,6 @@ const tf = require('@tensorflow/tfjs-node');
 const modelLink = 'https://storage.googleapis.com/bucketsubmission-1/model-in-prod/model.json'
 const envModelLink = `${process.env.MODEL_URL}`
 async function loadModel() {
-    return tf.loadGraphModel(envModelLink ?? modelLink);
+    return tf.'https://storage.googleapis.com/bucketsubmission-1/model-in-prod/model.json'(envModelLink ?? modelLink);
 }
 module.exports = loadModel;
