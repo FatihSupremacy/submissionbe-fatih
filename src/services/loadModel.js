443941842254d065fb39e270ca7c00402febdd50
Fatih Zaidan Aqil <167433085+FatihSupremacy@users.noreply.github.com>
2024-12-06 23:47:04 +0700

    Update loadModel.js

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,5 @@
 const tf = require('@tensorflow/tfjs-node');
-const modelLink = 'https://storage.googleapis.com/asclepius-dicoding-cc/model.json'
+const modelLink = 'https://storage.googleapis.com/bucketsubmission-1/model-in-prod/model.json'
 const envModelLink = `${process.env.MODEL_URL}`
 async function loadModel() {
     return tf.loadGraphModel(envModelLink ?? modelLink);
